d71fdd2143d18ddf934c347bfb8dd92444a21523
smthnspcl <pascal@eberlein.io>
2020-03-17 22:41:57 +0100

    updated variable names

--- Scany/libs/Device.py
+++ Scany/libs/Device.py
@@ -4,12 +4,12 @@ from datetime import datetime
 class Device(object):
     ip = None
     mac = None
-    firstconnect = None
-    lastconnect = None
+    first_connect = None
+    last_connect = None
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
     @staticmethod
-    def create(ip, mac):
-        return Device(ip=ip, mac=mac, firstconnect=datetime.now().strftime("%a, %H:%M:%S %d.%m.%Y"))
+    def create(ip, mac, date_format="%a, %H:%M:%S %d.%m.%Y"):
+        return Device(ip=ip, mac=mac, firstconnect=datetime.now().strftime(date_format))
